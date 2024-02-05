f27df4e0efd7b08d0dd6a0481f41987f32ded555
dohaduong <duonghado03@gmail.com>
2024-02-05 22:54:41 +0800

    Stats update: get total materials in last 5m, get materials for each event in last 5m, get total # of volunteers registered in last 5m, get total # of participants for each event in last 5m

--- be/controllers/statisticsController.js
+++ be/controllers/statisticsController.js
@@ -1,5 +1,53 @@
 const { supabase } = require("../config/supabaseClient");
 
+exports.getTotalVolunteers = async(req, res) => {
+    const { data, error } = await supabase
+        .rpc('gettotalvolunteer');
+    if (error) {
+        return res.status(500).json({ message: error.message });
+    }
+    if (!data) {
+        return res.status(500).json({ message: error.message });
+    }
+    return res.status(200).json({ totalVoluteer: data });
+}
+
+exports.getTotalMaterials = async(req, res) => {
+    const { data, error } = await supabase
+        .rpc('getmaterials');
+    if (error) {
+        return res.status(500).json({ message: error.message });
+    }
+    if (!data) {
+        return res.status(500).json({ message: error.message });
+    }
+    return res.status(200).json({ totalMaterials: data });
+}
+
+exports.getEventMaterials = async(req, res) => {
+    const { data, error } = await supabase
+        .rpc('getmaterialsfromeachevent');
+    if (error) {
+        return res.status(500).json({ message: error.message });
+    }
+    if (!data) {
+        return res.status(500).json({ message: error.message });
+    }
+    return res.status(200).json({ totalMaterials: data });
+}
+
+exports.getTotalEventPartcipant = async(req, res) => {
+    const { data, error } = await supabase
+        .rpc('gettotaleventparticipant');
+    if (error) {
+        return res.status(500).json({ message: error.message });
+    }
+    if (!data) {
+        return res.status(500).json({ message: error.message });
+    }
+    return res.status(200).json({ totalEventParticipant: data });
+}
+
 exports.getTotalStats = async (req, res) => {
     // const { data, error } = await supabase
     //     .from("outputs")
--- be/routes/statistics/statistics.js
+++ be/routes/statistics/statistics.js
@@ -2,6 +2,19 @@ const express = require('express');
 const router = express.Router();
 const statisticsController = require('../../controllers/statisticsController');
 
-router.get('/', statisticsController.getTotalStats);
+//total materials for ALL time
+router.get('/totalAllMaterials', statisticsController.getTotalStats); 
+
+//total materials for LAST 5 MONTHS: group by materials, order by totalQuantity desc
+router.get('/totalMaterials', statisticsController.getTotalMaterials); 
+
+//total materials for last 5m: group by (event, material type); order by totalQuantity desc
+router.get('/eventMaterial', statisticsController.getEventMaterials);
+
+//total # of volunteers registered in last 5m
+router.get('/totalVolunteers', statisticsController.getTotalVolunteers); 
+
+//total attendees, group by event, order by desc # of reviews
+router.get('/totalEventParticipant', statisticsController.getTotalEventPartcipant); 
 
 module.exports = router;
