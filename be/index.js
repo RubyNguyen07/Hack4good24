957b1ba5a33780b7916b33bf783061fbd9438537
Thang Pham <bathang02@gmail.com>
2024-02-06 19:00:49 +0800

    Fix be cors bug

--- be/index.js
+++ be/index.js
@@ -1,35 +1,33 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const formidable = require('express-formidable');
+const formidable = require("express-formidable");
 const cors = require("cors");
 
-
-const review = require('./routes/reviews/reviews');
-const volunteer = require('./routes/volunteers/volunteers');
-const campaign = require('./routes/campaigns/campaigns');
-const coordinator = require('./routes/coordinators/coordinators');
-const statistics = require('./routes/statistics/statistics');
+const review = require("./routes/reviews/reviews");
+const volunteer = require("./routes/volunteers/volunteers");
+const campaign = require("./routes/campaigns/campaigns");
+const coordinator = require("./routes/coordinators/coordinators");
+const statistics = require("./routes/statistics/statistics");
 const app = express();
 
 // using morgan for logs
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 app.use(formidable());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
-app.use('/reviews', review);
-app.use('/volunteers', volunteer);
-app.use('/campaigns', campaign);
-app.use('/coordinators', coordinator);
-app.use('/statistics', statistics);
+app.use("/reviews", review);
+app.use("/volunteers", volunteer);
+app.use("/campaigns", campaign);
+app.use("/coordinators", coordinator);
+app.use("/statistics", statistics);
 
-app.get('/', (req, res) => {
-    res.send("Hello I am working my friend Supabase <3");
+app.get("/", (req, res) => {
+  res.send("Hello I am working my friend Supabase <3");
 });
 
 app.listen(3000, () => {
-    console.log(`> Ready on http://localhost:3000`);
+  console.log(`> Ready on http://localhost:3000`);
 });
-
